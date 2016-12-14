d7a0d20c3da3ce87f9179f31cd6dc6a1c1fea9b7
Takashi Imagire <imagire@gmail.com>
2016-12-14 23:43:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ var users = require('./routes/users');
 
 var app = express();
 
+// 復号
+var password = "12345678901234567890123456789012";// 暗号化のキー
+var crypto = require("crypto");
+var decipher = crypto.createDecipher('aes192', password);
+decipher.update('94a4b41baacc417b5d5cc7df24bc3891', 'hex', 'utf8');// 第一引数が暗号化されたコード
+var dec = decipher.final('utf8');
+
 // BASIC 認証
 var basicAuth = require('basic-auth-connect');
-app.use(basicAuth('hoge', 'hogepass'));
+app.use(basicAuth('hoge', dec));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
