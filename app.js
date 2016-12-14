d4e0a8e69901a1143de727b30e5b0b2edf5a5e58
Takashi Imagire <imagire@gmail.com>
2016-12-15 00:03:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,11 @@ var generate_key = require('./routes/generate_key');
 var app = express();
 
 // 復号
-//var password = "12345678901234567890123456789012";// 暗号化のキー
-//var crypto = require("crypto");
-//var decipher = crypto.createDecipher('aes192', password);
-//decipher.update('94a4b41baacc417b5d5cc7df24bc3891', 'hex', 'utf8');// 第一引数が暗号化されたコード
-//var dec = decipher.final('utf8');
-var dec = 'hogepass';
+var password = '12345678901234567890123456789012';// 暗号化のキー
+var crypto = require('crypto');
+var decipher = crypto.createDecipher('aes192', password);
+decipher.update('94a4b41baacc417b5d5cc7df24bc3891', 'hex', 'utf8');// 第一引数が暗号化されたコード
+var dec = decipher.final('utf8');
 
 // BASIC 認証
 var basicAuth = require('basic-auth-connect');
