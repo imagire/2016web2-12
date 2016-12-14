11f9df8284970572e83b7f79674bd7c58566758a
Takashi Imagire <imagire@gmail.com>
2016-12-15 00:18:52 +0900

    Update generate_key.js

--- routes/generate_key.js
+++ routes/generate_key.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var app = require('../app.js');
 var crypto = require('crypto');
 
 /* 暗号化 */
 router.get('/:encrypt', function(req, res, next) {
-  var password = '12345678901234567890123456789012';// 暗号化のキー
+//  var password = '12345678901234567890123456789012';// 暗号化のキー
   var encrypt = req.params.encrypt;// URLから元の文字列を取得
   if(encrypt !== undefined)
   {
     // 暗号化
-    var cipher = crypto.createCipher('aes192', password);
+    var cipher = crypto.createCipher('aes192', global.password);
     cipher.update(encrypt, 'utf8', 'hex');
     var cipheredText = cipher.final('hex');
     res.send(cipheredText);
