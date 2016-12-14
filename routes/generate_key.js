4b609cd97dddcf54a254bc962200b4e687f29c60
Takashi Imagire <imagire@gmail.com>
2016-12-15 00:13:22 +0900

    Update generate_key.js

--- routes/generate_key.js
+++ routes/generate_key.js
@@ -4,9 +4,6 @@ var crypto = require('crypto');
 
 /* 暗号化 */
 router.get('/:encrypt', function(req, res, next) {
-    res.send(req.params.encrypt);
-    return;
-
   var password = '12345678901234567890123456789012';// 暗号化のキー
 
   var encrypt = req.params.encrypt;// URLから元の文字列を取得
